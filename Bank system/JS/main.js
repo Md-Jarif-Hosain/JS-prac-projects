f398b0305bd3dcd0f4677b33752cc374871fb37d
Jarif Hosain <hosainjarif@gmail.com>
2022-04-26 23:32:03 +0600

    add arrow function

--- Bank system/JS/main.js
+++ Bank system/JS/main.js
@@ -1,7 +1,7 @@
 
 // login button event handler
 const loginBtn = document.getElementById('login');
-loginBtn.addEventListener('click', function () {
+loginBtn.addEventListener('click', () => {
     const loginArea = document.getElementById('login-area');
     const transactionArea = document.getElementById('transaction-are');
     loginArea.style.display = 'none';
@@ -10,19 +10,23 @@ loginBtn.addEventListener('click', function () {
 
 // Deposit button event handler
 const depositBtn = document.getElementById('add-deposit');
-depositBtn.addEventListener('click', function () {
+depositBtn.addEventListener('click', () => {
 
     const depositNumber = getInputNumber('deposit-amount');
 
-    updateSpanText("current-deposit", depositNumber);
-    updateSpanText("current-balance", depositNumber);
+    if (depositNumber < 0) {
+        alert("Deposit number cannot be negative");
+    } else {
+        updateSpanText("current-deposit", depositNumber);
+        updateSpanText("current-balance", depositNumber);
 
 
-    document.getElementById('deposit-amount').value = "";
+        document.getElementById('deposit-amount').value = "";
+    }
 });
 
 // withdraw button event handler
-const withdrawBtn = document.getElementById('add-withdraw'); withdrawBtn.addEventListener('click', function () {
+const withdrawBtn = document.getElementById('add-withdraw'); withdrawBtn.addEventListener('click', () => {
 
     const withdrawNumber = getInputNumber('withdraw-amount');
 
